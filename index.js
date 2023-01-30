676b28df716f72038e3fb0c99667d153e55a3b21
heorhii <georgeovod@gmail.com>
2023-01-30 22:44:35 +0100

    add sum, substraction, multiplay

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const sum = (num1, num2) => {
+    let result = num1 + num2;
+    return result;
+};
+
+const subtraction = (num1, num2) => {
+    let result = num1 - num2;
+    return result;
+};
+
+const multiplay = (num1, num2) => {
+    let result = num1 * num2;
+    return result;
+};
